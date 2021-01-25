08b1c0d42e638a34d77eb46cf6cc1b8bb3963db6
Manuel Duran <durbonca@gmail.com>
2021-01-25 19:49:46 -0300

    cerrar la nota activa

--- src/components/ActiveNote.vue
+++ src/components/ActiveNote.vue
@@ -3,7 +3,8 @@
     <section class="flex-1 |">
       <textarea
         v-if="activeNote"
-        :value="activeNote"
+        :value="activeNote.body"
+        @input="updateNote"
         class="w-full h-full | bg-gray-200 | p-3"
         name=""
         id=""
@@ -24,12 +25,19 @@ export default {
   name: "ActiveNote",
   setup() {
     const store = useStore();
+    const activeNote = computed(() =>
+      store.state.activeNote
+        ? store.getters.getNoteById(store.state.activeNote)
+        : null
+    );
+    const updateNote = $event =>
+      store.commit("updateNote", {
+        id: activeNote.value.id,
+        body: $event.target.value
+      });
     return {
-      activeNote: computed(() =>
-        store.state.activeNote
-          ? store.getters.getNoteById(store.state.activeNote).body
-          : null
-      )
+      activeNote,
+      updateNote
     };
   }
 };
