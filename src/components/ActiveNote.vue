18b0bfbd68906a47bdb00c611e7f1dd95e38f9cf
Manuel Duran <durbonca@gmail.com>
2021-01-30 20:10:46 -0300

    extrapolar a componente activenotehtml

--- src/components/ActiveNote.vue
+++ src/components/ActiveNote.vue
@@ -14,10 +14,10 @@
           rows="10"
         ></textarea>
       </section>
-      <article
-        v-html="markdown"
-        class="mdscss bg-gray-900 text-white | p-3 | flex-1"
-      ></article>
+      <ActiveNoteHTML
+        class="bg-gray-900 text-white | p-3 | flex-1"
+        :body="activeNote.body"
+      />
     </div>
     <!-- notes info and actions -->
     <section class="mt-3 | flex justify-end">
@@ -34,10 +34,13 @@
 <script>
 import { computed } from "vue";
 import { useStore } from "vuex";
+import ActiveNoteHTML from "@/components/ActiveNoteHTML.vue";
 export default {
   name: "ActiveNote",
+  components: {
+    ActiveNoteHTML
+  },
   setup() {
-    const marked = require("marked");
     const store = useStore();
     const activeNote = computed(() =>
       store.state.activeNote
@@ -50,74 +53,12 @@ export default {
         body: $event.target.value
       });
 
-    const markdown = computed(() => {
-      return activeNote.value ? marked(activeNote.value.body) : null;
-    });
-
     const closeNote = () => store.commit("setActiveNote");
     return {
       activeNote,
       updateNote,
-      closeNote,
-      markdown
+      closeNote
     };
   }
 };
 </script>
-
-<style>
-.mdscss {
-  font-size: 1rem;
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
-    "Roboto", "Helvetica Neue", Arial, sans-serif;
-}
-.mdscss h1 {
-  font-size: 3.375em;
-  font-weight: bold;
-}
-.mdscss h2 {
-  font-size: 2.25em;
-  font-weight: bold;
-}
-.mdscss h3 {
-  font-size: 1.5em;
-  font-weight: bold;
-}
-.mdscss h4 {
-  font-size: 1rem;
-  font-weight: bold;
-}
-.mdscss strong {
-  font-weight: bold;
-}
-.mdscss em {
-  font-style: italic;
-}
-.mdscss small {
-  font-size: 0.667em;
-}
-.mdscss code {
-  font-family: Consolas, monaco, monospace;
-}
-.mdscss li {
-  line-height: 1.5em;
-}
-.mdscss p {
-  line-height: 1.5em;
-}
-.mdscss blockquote {
-  margin-left: 0;
-  padding-left: 0.5rem;
-  border-left: 0.5rem solid #ddd;
-}
-.mdscss pre {
-  background: #f8f8f8;
-  padding: 1em;
-}
-.mdscss * + * {
-  margin-top: 1.5rem;
-}
-.mdscss li + li {
-  margin-top: 0;
-}
-</style>
