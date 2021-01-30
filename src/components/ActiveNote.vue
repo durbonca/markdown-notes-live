2c8fd0615e87693fb863af6f529eb3fecf8efd1d
Manuel Duran <durbonca@gmail.com>
2021-01-30 19:34:53 -0300

    convertir markdown en html

--- src/components/ActiveNote.vue
+++ src/components/ActiveNote.vue
@@ -14,9 +14,10 @@
           rows="10"
         ></textarea>
       </section>
-      <article class="bg-gray-900 text-white | p-3 | flex-1">
-        HTML will be here
-      </article>
+      <article
+        v-html="markdown"
+        class="mdscss bg-gray-900 text-white | p-3 | flex-1"
+      ></article>
     </div>
     <!-- notes info and actions -->
     <section class="mt-3 | flex justify-end">
@@ -36,6 +37,7 @@ import { useStore } from "vuex";
 export default {
   name: "ActiveNote",
   setup() {
+    const marked = require("marked");
     const store = useStore();
     const activeNote = computed(() =>
       store.state.activeNote
@@ -47,12 +49,75 @@ export default {
         id: activeNote.value.id,
         body: $event.target.value
       });
+
+    const markdown = computed(() => {
+      return activeNote.value ? marked(activeNote.value.body) : null;
+    });
+
     const closeNote = () => store.commit("setActiveNote");
     return {
       activeNote,
       updateNote,
-      closeNote
+      closeNote,
+      markdown
     };
   }
 };
 </script>
+
+<style>
+.mdscss {
+  font-size: 1rem;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
+    "Roboto", "Helvetica Neue", Arial, sans-serif;
+}
+.mdscss h1 {
+  font-size: 3.375em;
+  font-weight: bold;
+}
+.mdscss h2 {
+  font-size: 2.25em;
+  font-weight: bold;
+}
+.mdscss h3 {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+.mdscss h4 {
+  font-size: 1rem;
+  font-weight: bold;
+}
+.mdscss strong {
+  font-weight: bold;
+}
+.mdscss em {
+  font-style: italic;
+}
+.mdscss small {
+  font-size: 0.667em;
+}
+.mdscss code {
+  font-family: Consolas, monaco, monospace;
+}
+.mdscss li {
+  line-height: 1.5em;
+}
+.mdscss p {
+  line-height: 1.5em;
+}
+.mdscss blockquote {
+  margin-left: 0;
+  padding-left: 0.5rem;
+  border-left: 0.5rem solid #ddd;
+}
+.mdscss pre {
+  background: #f8f8f8;
+  padding: 1em;
+}
+.mdscss * + * {
+  margin-top: 1.5rem;
+}
+.mdscss li + li {
+  margin-top: 0;
+}
+</style>
