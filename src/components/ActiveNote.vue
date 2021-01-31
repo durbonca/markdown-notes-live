56c146ab0b00c6de07086c4944cead77e7412d77
Manuel Duran <durbonca@gmail.com>
2021-01-31 08:04:32 -0300

    crear una nota

--- src/components/ActiveNote.vue
+++ src/components/ActiveNote.vue
@@ -2,17 +2,11 @@
   <div v-if="activeNote" class="h-full | flex  flex-col ">
     <!-- edition actions -->
     <div class="flex-1 flex">
-      <section class="flex-1 | flex ">
-        <textarea
-          v-if="activeNote"
-          :value="activeNote.body"
-          @input="updateNote"
+      <section class="flex-1">
+        <ActiveNoteMD
+          v-model:body="activeNote.body"
           class="w-full h-full | bg-gray-200 | p-3"
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-        ></textarea>
+        />
       </section>
       <ActiveNoteHTML
         class="bg-gray-900 text-white | p-3 | flex-1"
@@ -21,13 +15,17 @@
     </div>
     <!-- notes info and actions -->
     <section class="mt-3 | flex justify-end">
-      <a href="" @click="closeNote" class="bg-gray-200 py-1 px-3 rounded-md"
+      <a href="#" @click="closeNote" class="bg-gray-200 py-1 px-3 rounded-md"
         >Close Note</a
       >
     </section>
   </div>
   <div v-else class="h-full | flex justify-center items-center ">
-    please select a note to start editing ✍
+    please select a note to start editing or
+    <a @click="createNote" class="undeline font-bold" href="#"
+      >&nbsp; create a note
+    </a>
+    &nbsp; ✍
   </div>
 </template>
 
@@ -35,10 +33,12 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
 import ActiveNoteHTML from "@/components/ActiveNoteHTML.vue";
+import ActiveNoteMD from "@/components/ActiveNoteMD.vue";
 export default {
   name: "ActiveNote",
   components: {
-    ActiveNoteHTML
+    ActiveNoteHTML,
+    ActiveNoteMD
   },
   setup() {
     const store = useStore();
@@ -52,12 +52,13 @@ export default {
         id: activeNote.value.id,
         body: $event.target.value
       });
-
     const closeNote = () => store.commit("setActiveNote");
+    const createNote = () => store.dispatch("createNote");
     return {
       activeNote,
       updateNote,
-      closeNote
+      closeNote,
+      createNote
     };
   }
 };
---
+++ src/components/ActiveNoteMD.vue
@@ -0,0 +1,28 @@
+<template>
+  <textarea
+    ref="textarea"
+    :value="body"
+    @input="$emit('update:body', $event.target.value)"
+  ></textarea
+></template>
+
+<script>
+import { ref, onMounted, onUpdated } from "vue";
+export default {
+  name: "ActiveNoteMD",
+  emits: ["update:body"],
+  props: {
+    body: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    const textarea = ref(null);
+    const focusTextarea = () => textarea.value.focus();
+    onMounted(focusTextarea);
+    onUpdated(focusTextarea);
+    return { textarea };
+  }
+};
+</script>
